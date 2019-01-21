bc2fb5af1e29d005731015f2f148ca9ba7b26222
Kw11 <alexandre.caila@epitech.eu>
2019-01-21 20:59:14 +0100

    system almost done

--- src/GLoop.hpp
+++ src/GLoop.hpp
@@ -1,10 +1,30 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+#include "System.hpp"
+
 namespace gloop {
+
+	using family_type = uint32_t;
 
 	class GLoop {
 		public:
+			GLoop() = default;
+			~GLoop() = default;
+
+			void	run();
+
+
+			void	addSystem();
+			void	removeSystem();
 
 		private:
+			void	init();
+
+			void	update();
+
+		private:
+	//		SparseSet<ISystem>	_systems;
 	};
 };
---
+++ src/System.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+namespace gloop {
+
+	class ISystem {
+		public:
+			virtual ~ISystem() = default;
+
+			virtual void	init() = 0;
+			virtual void	update() = 0;
+
+			virtual const uint32_t	&getPriority() const noexcept = 0;
+			virtual void		setPriority(const uint32_t) = 0;
+
+			virtual const int		&getCallrate() const noexcept = 0;
+			virtual void		setCallrate(const int) const noexcept = 0;
+	};
+};
