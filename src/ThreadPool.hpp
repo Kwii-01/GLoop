0043dd33f25a0c80ed4fb731efcd8d59eb239a63
Kw11 <alexandre.caila@epitech.eu>
2019-01-22 14:28:55 +0100

    threadpool in wokr

--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <thread>
+#include <mutex>
+#include <queue>
+
 namespace gloop {
 
 	class ThreadPool {
 		public:
+			ThreadPool(const std::size_t max_worker);
 
 		private:
+			std::mutex			_mutexWorker;
 	};
 };
